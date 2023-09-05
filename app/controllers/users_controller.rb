70e5fab3d62cb11460b4f6e310c58fee12048806
Giovanna Perri <giovannaperri@outlook.com>
2023-09-05 14:28:18 -0300

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @users = User.where("name LIKE ?", "%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @users = User.all
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
