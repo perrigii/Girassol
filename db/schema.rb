359db6366beaafcad814fcad420c86051800a6e5
diego joao <diegosgjoao@gmail.com>
2023-09-05 12:02:09 -0300

    adicionado campo de especialidade ao model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_140444) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_145952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_140444) do
     t.string "surname"
     t.date "birthdate"
     t.text "description"
+    t.string "specialty"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
