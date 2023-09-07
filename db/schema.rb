68296c4b45fce8582af982acccd78ccbb2c8f69a
André ÃLacerda <dedelacerda95@gmail.com>
2023-09-07 11:16:29 -0300

    vamos testar a video chamada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_202409) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_140544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_202409) do
     t.datetime "updated_at", null: false
     t.bigint "appointment_id", null: false
     t.index ["appointment_id"], name: "index_reviews_on_appointment_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.string "session_token"
+    t.string "generated_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
