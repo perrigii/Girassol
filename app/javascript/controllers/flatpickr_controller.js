e97f80ba9cbac9c67fc925673b64a30327e0813c
Giovanna Perri <giovannaperri@outlook.com>
2023-09-08 14:24:08 -0300

    calendario no show de therapist

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,12 +1,33 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-// Connects to data-controller="flatpickr"
 export default class extends Controller {
-  static targets = [ "startTime", "endTime" ]
-
   connect() {
-    flatpickr(this.startTimeTarget, {})
-    flatpickr(this.endTimeTarget, {})
+    new flatpickr(this.element, {
+      altInput: true,
+      altFormat: "j F, Y",
+      dateFormat: "d.m.Y",
+      minDate: "today",
+      inline: true,
+      // plugins: [new confirmDatePlugin({})]
+      // dateFormat: "l",
+      // more options available on the documentation!
+    });
+
+    flatpickr("#end_time", {
+      altInput: true,
+
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: "H:i",
+      minTime: "08:00",
+      maxTime: "17:30",
+      minDate: "today",
+      inline: true,
+      time_24hr: true,
+
+    });
+
   }
 }
