ffa2a6b14a9b574c732fd2db6d7d7aa6cf64589e
André ÃLacerda <dedelacerda95@gmail.com>
2023-09-13 12:21:29 -0300

    criei pop up do dedechat no link de criar sala da navbar

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,17 @@
 class RoomsController < ApplicationController
+  def new
+    @room = Room.new
+  end
+
   def create
     @room = Room.find_or_create_by(room_params)
     if @room.id
-      redirect_to room_path(@room)
+      respond_to do |format|
+        format.turbo_stream {redirect_to room_path(@room)}
+        format.html {redirect_to room_path(@room)}
+          # render turbo_stream: turbo_stream.replace("home", partial: "rooms/show", locals: {room: @room})
+          # render turbo_stream: turbo_stream.redirect(room_path(@room))
+      end
     else
       redirect_to root_path
     end
